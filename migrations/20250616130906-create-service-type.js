c8572524e9ec81b36edc548a9a439d40f5e530d1
miguelj24 <pena87150@gmail.com>
2025-09-17 21:07:41 -0500

    very changes

--- migrations/20250616130906-create-service-type.js
+++ migrations/20250616130906-create-service-type.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('serviceTypes', {
+    await queryInterface.createTable('servicetypes', {
       id: {
         allowNull: false,
         autoIncrement: true,
