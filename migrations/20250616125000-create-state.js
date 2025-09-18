d1fc41cf7efb5fd6962c08415cc5507c7fabf5bc
miguelj24 <pena87150@gmail.com>
2025-09-17 21:04:02 -0500

    changes

--- migrations/20250616125000-create-state.js
+++ migrations/20250616125000-create-state.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('States', {
+    await queryInterface.createTable('states', {
       id: {
         allowNull: false,
         autoIncrement: true,
