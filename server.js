1973d6130fc689b8f26e019a11416eece416d9b8
miguelj24 <pena87150@gmail.com>
2025-09-29 15:40:55 -0500

    cors configurated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); // Se llama la depenedencia del Framework
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
@@ -10,6 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.set('port', process.env.PORT || 4000);
 
+app.use(cors()); 
 app.use(morgan('dev'));
 // Permite recibir datos del formulario
 app.use(bodyParser.urlencoded({ extended: false}));
